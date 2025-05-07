11d806f2e9e5f23981d24a6c56ae5b3903b87e96
Hamshayini_Thavarajah <139700236+HamshayiniThavarajah@users.noreply.github.com>
2025-05-07 10:40:07 +0530

    main src file is updated

---
+++ src/main/java/lk/vau/fas/ict/dayeight/DayeightApplication.java
@@ -0,0 +1,13 @@
+package lk.vau.fas.ict.dayeight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayeightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayeightApplication.class, args);
+	}
+
+}
